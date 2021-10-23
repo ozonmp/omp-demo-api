2961bfee29197964cb53866aeac37eda0cffa9c9
Artyom Tetyukhin <51746822+arttet@users.noreply.github.com>
2021-10-23 17:18:25 +0300

    Fix the bug related to release resources

--- internal/app/retranslator/retranslator_test.go
+++ internal/app/retranslator/retranslator_test.go
@@ -11,20 +11,22 @@ import (
 func TestStart(t *testing.T) {
 
 	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
 	repo := mocks.NewMockEventRepo(ctrl)
 	sender := mocks.NewMockEventSender(ctrl)
 
 	repo.EXPECT().Lock(gomock.Any()).AnyTimes()
 
 	cfg := Config{
-		ChannelSize:   512,
-		ConsumerCount: 2,
-		ConsumeSize:   10,
+		ChannelSize:    512,
+		ConsumerCount:  2,
+		ConsumeSize:    10,
 		ConsumeTimeout: 10 * time.Second,
-		ProducerCount: 2,
-		WorkerCount:   2,
-		Repo:          repo,
-		Sender:        sender,
+		ProducerCount:  2,
+		WorkerCount:    2,
+		Repo:           repo,
+		Sender:         sender,
 	}
 
 	retranslator := NewRetranslator(cfg)
