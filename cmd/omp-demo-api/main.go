3c65429392170e82a3b1d8090628307194c3d98f
Vyacheslav Startsev <vyacheslav.startsev@skyeng.ru>
2021-10-24 13:23:38 +0700

    Fix consumer timeout

--- cmd/omp-demo-api/main.go
+++ cmd/omp-demo-api/main.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/ozonmp/omp-demo-api/internal/app/retranslator"
 )
@@ -13,11 +14,12 @@ func main() {
 	sigs := make(chan os.Signal, 1)
 
 	cfg := retranslator.Config{
-		ChannelSize:   512,
-		ConsumerCount: 2,
-		ConsumeSize:   10,
-		ProducerCount: 28,
-		WorkerCount:   2,
+		ChannelSize:    512,
+		ConsumerCount:  2,
+		ConsumeSize:    10,
+		ConsumeTimeout: 10 * time.Second,
+		ProducerCount:  28,
+		WorkerCount:    2,
 	}
 
 	retranslator := retranslator.NewRetranslator(cfg)
